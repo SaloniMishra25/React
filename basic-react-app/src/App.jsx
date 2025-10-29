8f77800349ad6b298294ae5288e415743d9c8da1
SaloniMishra25 <salonimishra0087@gmail.com>
2025-10-29 16:51:11 +0530

    first component

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
+import Title from "./Title";
+
 
 function App() {
-  
-
   return (
     <>
-      <h1>Hello React</h1>
+      <h2>Welcome Back!</h2>
+      <Title />
+      <p>Software Developer</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
